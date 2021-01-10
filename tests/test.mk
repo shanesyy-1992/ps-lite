6d4a4b43dc646de74b60e9bbd30f8ae0a1b81f8d
Yulu Jia <yulu.jia@bytedance.com>
2021-01-10 06:30:13 +0000

    run test_benchmark on ucx+gdr
    Signed-off-by: Yulu Jia <yulu.jia@bytedance.com>

--- tests/test.mk
+++ tests/test.mk
@@ -4,7 +4,7 @@ TEST = $(patsubst tests/test_%.cc, tests/test_%, $(TEST_SRC))
 # -ltcmalloc_and_profiler
 LDFLAGS = -Wl,-rpath,$(DEPS_PATH)/lib $(PS_LDFLAGS_SO) -pthread -lcudart
 tests/% : tests/%.cc build/libps.a
-	$(CXX) -std=c++0x $(CFLAGS) -MM -MT tests/$* $< >tests/$*.d $(LIBS)
-	$(CXX) -std=c++0x $(CFLAGS) -o $@ $(filter %.cc %.a, $^) $(LDFLAGS) $(LIBS)
+	$(CXX) -std=c++0x $(CFLAGS) $(ADD_TEST_CFLAGS) -MM -MT tests/$* $< >tests/$*.d $(LIBS)
+	$(CXX) -std=c++0x $(CFLAGS) $(ADD_TEST_CFLAGS) -o $@ $(filter %.cc %.a, $^) $(LDFLAGS) $(LIBS)
 
 -include tests/*.d
